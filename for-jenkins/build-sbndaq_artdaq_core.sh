62eee9784e8dd27ab2d5ddd390a03cc3f010df49
Gennadiy <lukhanin@fnal.gov>
2020-06-11 03:30:17 -0500

    Uppercase the artdaq_version variable.

--- for-jenkins/build-sbndaq_artdaq_core.sh
+++ for-jenkins/build-sbndaq_artdaq_core.sh
@@ -151,7 +151,7 @@ echo
 cd ${products_dir} || exit 1
 curl --fail --silent --location --insecure -O http://scisoft.fnal.gov/scisoft/bundles/tools/pullProducts || exit 1
 chmod +x pullProducts
-./pullProducts ${products_dir} ${flvr} artdaq-${artdaq_version} ${manifest_qual_set//:/-} ${build_type} 2>&1 |tee ${products_dir}/pullproducts.log
+./pullProducts ${products_dir} ${flvr} artdaq-${ARTDAQ_VERSION} ${manifest_qual_set//:/-} ${build_type} 2>&1 |tee ${products_dir}/pullproducts.log
 
 
 
