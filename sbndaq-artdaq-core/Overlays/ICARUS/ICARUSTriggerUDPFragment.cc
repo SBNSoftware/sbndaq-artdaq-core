b817378a06a223d0cbd6dbd77390e3235abd0cb6
Zettlemoyer, Jacob <jzettle@fnal.gov>
2021-05-12 12:01:33 -0500

    Adding more printing information for the trigger fragment to test fragment dump module

--- sbndaq-artdaq-core/Overlays/ICARUS/ICARUSTriggerUDPFragment.cc
+++ sbndaq-artdaq-core/Overlays/ICARUS/ICARUSTriggerUDPFragment.cc
@@ -6,6 +6,20 @@
 std::ostream & icarus::operator << (std::ostream & os, ICARUSTriggerUDPFragment const & f) {
  
   os << "ICARUSTriggerUDPFragment: " << f.GetDataString();
+  os << "\n\t Event Number: " << f.getWRName();
+  os << "\n\t Seconds since UTC epoch: " << f.getWRSeconds();
+  os << "\n\t Nanoseconds: " << f.getWRNanoSeconds();
+  os << "\n\t Gate ID: " << f.getGateID();
+  os << "\n\t Gate Type: " << f.getGateType();
+  os << "\n\t isBNB: " << f.isBNB();
+  os << "\n\t isNuMI: " << f.isNuMI();
+  os << "\n\t Last Overall Timestamp: " << f.getLastTimestamp();
+  os << "\n\t Last BNB Timestamp: " << f.getLastTimestampBNB();
+  os << "\n\t Last NuMI Timestamp: " << f.getLastTimestampNuMI();
+  os << "\n\t Delta Gates: " << f.getDeltaGates();
+  os << "\n\t Delta Gates BNB: " << f.getDeltaGatesBNB();
+  os << "\n\t Delta Gates NuMI: " << f.getDeltaGatesNuMI();
+  
   os << std::endl;
   return os;
 }
