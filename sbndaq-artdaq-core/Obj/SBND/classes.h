0330583e471ffd160000140f591a12ee7363953a
Patrick Green <patrickgreen135@gmail.com>
2022-03-11 13:23:35 -0600

    pmtTrigger class moved out of sbndaq

--- sbndaq-artdaq-core/Obj/SBND/classes.h
+++ sbndaq-artdaq-core/Obj/SBND/classes.h
@@ -1,7 +1,6 @@
 #include "canvas/Persistency/Common/Wrapper.h"
 #include "canvas/Persistency/Common/Assns.h"
 #include "sbndaq-artdaq-core/Obj/SBND/testStandTrigger.h"
-#include "sbndaq-artdaq-core/Obj/SBND/pmtTrigger.h"
 #include <vector>
 #include <map>
 #include <utility>
--- sbndaq-artdaq-core/Obj/SBND/pmtTrigger.cc
+++
@@ -1,3 +0,0 @@
-#include "sbndaq-artdaq-core/Obj/SBND/pmtTrigger.h"
-
-// nothing here yet
