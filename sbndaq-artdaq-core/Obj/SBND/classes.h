0f6c7df4d0503060f1d8256780af0046302cf2fe
sbnd <sbnd@fnal.gov>
2022-08-23 14:26:35 -0500

    fix CRTmetric.hh name

--- sbndaq-artdaq-core/Obj/SBND/classes.h
+++ sbndaq-artdaq-core/Obj/SBND/classes.h
@@ -1,7 +1,7 @@
 #include "canvas/Persistency/Common/Wrapper.h"
 #include "canvas/Persistency/Common/Assns.h"
 #include "sbndaq-artdaq-core/Obj/SBND/testStandTrigger.h"
-#include "sbndaq-artdaq-core/Obj/SBND/CRTmetric.h"
+#include "sbndaq-artdaq-core/Obj/SBND/CRTmetric.hh"
 #include <vector>
 #include <map>
 #include <utility>
