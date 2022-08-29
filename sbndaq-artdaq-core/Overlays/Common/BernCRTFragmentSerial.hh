0c0320321969f90fb3e321401a5ae95e731e72e5
Henry Lay <h.lay@lancaster.ac.uk>
2022-08-29 14:25:45 -0500

    Serialize fragment type

--- sbndaq-artdaq-core/Overlays/Common/BernCRTFragmentSerial.hh
+++ sbndaq-artdaq-core/Overlays/Common/BernCRTFragmentSerial.hh
@@ -4,6 +4,7 @@ namespace sbndaq {
 
   class BernCRTFragmentSerial {
   public:
+    artdaq::detail::RawFragmentHeader::type_t        fragment_type;
     artdaq::detail::RawFragmentHeader::sequence_id_t sequence_id;
     artdaq::detail::RawFragmentHeader::fragment_id_t fragment_id;
     artdaq::detail::RawFragmentHeader::timestamp_t   timestamp;
@@ -14,6 +15,7 @@ namespace sbndaq {
     template<class Archive>
     void serialize(Archive& ar, const unsigned int /*version*/)
     {
+      ar & fragment_type;
       ar & sequence_id;
       ar & fragment_id;
       ar & timestamp;
