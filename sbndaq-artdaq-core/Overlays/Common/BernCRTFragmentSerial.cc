52c061366acf394f7c1d753edd61ffdc0aa585d8
Henry Lay <h.lay@lancaster.ac.uk>
2022-08-30 17:54:16 -0500

    Better format the print statement

--- sbndaq-artdaq-core/Overlays/Common/BernCRTFragmentSerial.cc
+++ sbndaq-artdaq-core/Overlays/Common/BernCRTFragmentSerial.cc
@@ -5,9 +5,9 @@ std::ostream& sbndaq::operator<<(std::ostream& os, BernCRTFragmentSerial const& 
   FragmentSerialBase serial_base(serial);
 
   os << serial_base
-     << "Data Payload Size: " << serial.data_payload_size
-     << "Number of Hits:    " << serial.n_hits
-     << serial.metadata;
+     << "Data Payload Size: " << serial.data_payload_size << '\n'
+     << "Number of Hits:    " << serial.n_hits << '\n'
+     << serial.metadata << '\n';
 
   for(unsigned hit = 0; hit < serial.bern_crt_hits.size(); ++hit)
     os << "Hit: " << hit << '\n' << serial.bern_crt_hits.at(hit);
