b64fdbd2bc451b6052be154563ff7a0575ea8b76
Henry Lay <h.lay@lancaster.ac.uk>
2022-08-31 09:53:55 -0500

    Create CAEN fragment serial

---
+++ sbndaq-artdaq-core/Overlays/Common/CAENV1730FragmentSerial.cc
@@ -0,0 +1,12 @@
+#include "sbndaq-artdaq-core/Overlays/Common/CAENV1730FragmentSerial.hh"
+
+std::ostream& sbndaq::operator<<(std::ostream& os, CAENV1730FragmentSerial const& serial)
+{
+  FragmentSerialBase serial_base(serial);
+
+  os << serial_base
+     << serial.metadata << '\n'
+     << serial.event << '\n';
+
+  return os;
+}
---
+++ sbndaq-artdaq-core/Overlays/Common/CAENV1730FragmentSerial.hh
@@ -0,0 +1,34 @@
+#ifndef sbndaq_artdaq_core_Overlays_Common_CAENV1730FragmentSerial_hh
+#define sbndaq_artdaq_core_Overlays_Common_CAENV1730FragmentSerial_hh
+
+#include "artdaq-core/Data/detail/RawFragmentHeader.hh"
+#include "artdaq-core/Data/Fragment.hh"
+#include "sbndaq-artdaq-core/Overlays/Common/FragmentSerialBase.hh"
+#include "sbndaq-artdaq-core/Overlays/Common/CAENV1730Fragment.hh"
+
+#include <boost/serialization/base_object.hpp>
+
+namespace sbndaq {
+
+  class CAENV1730FragmentSerial;
+  std::ostream& operator<<(std::ostream&, CAENV1730FragmentSerial const&);
+
+  class CAENV1730FragmentSerial : public sbndaq::FragmentSerialBase {
+  public:
+    CAENV1730FragmentMetadata metadata;
+    CAENV1730Event event;
+    
+  private:
+    friend class boost::serialization::access;
+
+    template<class Archive>
+    void serialize(Archive& ar, const unsigned int /*version*/)
+    {
+      ar & boost::serialization::base_object<FragmentSerialBase>(*this);
+      ar & metadata;
+      ar & event;
+    }
+  };
+}
+
+#endif
