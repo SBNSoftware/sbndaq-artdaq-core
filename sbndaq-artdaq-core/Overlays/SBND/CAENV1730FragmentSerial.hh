fd7f34c0b8177777c327dac85e36c6196269b323
Henry Lay <h.lay@lancaster.ac.uk>
2022-09-11 21:30:41 -0500

    Consistence-ify variable name

--- sbndaq-artdaq-core/Overlays/SBND/CAENV1730FragmentSerial.hh
+++ sbndaq-artdaq-core/Overlays/SBND/CAENV1730FragmentSerial.hh
@@ -18,7 +18,7 @@ namespace sbndaq {
   public:
     CAENV1730FragmentMetadata metadata;
     CAENV1730Event event;
-    std::vector<std::vector<uint16_t> > fWvfmsVec;
+    std::vector<std::vector<uint16_t> > wvfmvec;
     
   private:
     friend class boost::serialization::access;
@@ -29,7 +29,7 @@ namespace sbndaq {
       ar & boost::serialization::base_object<FragmentSerialBase>(*this);
       ar & metadata;
       ar & event;
-      ar & fWvfmsVec;
+      ar & wvfmvec;
     }
   };
 }
