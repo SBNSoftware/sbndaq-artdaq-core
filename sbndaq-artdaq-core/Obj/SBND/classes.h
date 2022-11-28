610713cbce0baeac3b8dda26d60fd4a0fdfabdc5
sbnd <sbnd@fnal.gov>
2022-11-28 10:47:05 -0600

    add pmt software trigger metrics

--- sbndaq-artdaq-core/Obj/SBND/classes.h
+++ sbndaq-artdaq-core/Obj/SBND/classes.h
@@ -2,6 +2,7 @@
 #include "canvas/Persistency/Common/Assns.h"
 #include "sbndaq-artdaq-core/Obj/SBND/testStandTrigger.h"
 #include "sbndaq-artdaq-core/Obj/SBND/CRTmetric.hh"
+#include "sbndaq-artdaq-core/Obj/SBND/pmtSoftwareTrigger.hh"
 #include <vector>
 #include <map>
 #include <utility>
