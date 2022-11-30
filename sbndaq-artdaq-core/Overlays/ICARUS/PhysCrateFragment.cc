6d8b4b72d43c0fe0450d0b024857bd4600a610b0
Harry Hausner <hhausner@icarus-gateway01.fnal.gov>
2022-11-30 12:10:13 -0600

    update the ICARUS PhysCrateFragments to handle compressed data

--- sbndaq-artdaq-core/Overlays/ICARUS/PhysCrateFragment.cc
+++ sbndaq-artdaq-core/Overlays/ICARUS/PhysCrateFragment.cc
@@ -59,6 +59,18 @@ icarus::A2795DataBlock::header_t icarus::PhysCrateFragment::BoardTimeStamp(uint1
 
 icarus::A2795DataBlock::data_t const* icarus::PhysCrateFragment::BoardData(uint16_t b) const{
   //return (reinterpret_cast<A2795DataBlock::data_t const *>(BoardDataBlock(b)->data));
+  if(isCompressed())
+  {
+    size_t cumulativeSize = (b == 0) ? 0 : cumulativeBoardSize(b - 1);
+    return ( reinterpret_cast<A2795DataBlock::data_t const*>
+             (artdaq_Fragment_.dataBeginBytes()
+              + sizeof(PhysCrateDataTileHeader)
+              + sizeof(A2795DataBlock::Header)
+              + b*sizeof(PhysCrateDataTileHeader)
+              + b*4*sizeof(uint16_t)
+              + cumulativeSize));
+  }
+
   return ( reinterpret_cast< A2795DataBlock::data_t const *>
 	   (artdaq_Fragment_.dataBeginBytes() + sizeof(PhysCrateDataTileHeader)
 	    + b*(sizeof(PhysCrateDataTileHeader) + 4*sizeof(uint16_t) + BoardBlockSize())
@@ -66,6 +78,10 @@ icarus::A2795DataBlock::data_t const* icarus::PhysCrateFragment::BoardData(uint1
 }
 
 icarus::A2795DataBlock::data_t icarus::PhysCrateFragment::adc_val(size_t b,size_t c, size_t s) const{
+  if (isCompressed())
+    return adc_val_recursive_helper(b, c, 0, s,
+             std::make_pair(static_cast<icarus::A2795DataBlock::data_t>(0), BoardData(b))).first;
+
   return ( *(BoardData(b)+s*nChannelsPerBoard()+c) & (~(1<<(metadata()->num_adc_bits()+1))) );
 }
 
@@ -160,15 +176,22 @@ std::ostream & icarus::operator << (std::ostream & os, PhysCrateFragment const &
 bool icarus::PhysCrateFragment::Verify() const {
   bool verified=true;
 
-  if(!isCompressed()){
-    TRACE(TLVL_INFO,"Data is compressed, so no verification done.");
-    return verified;
-  }
+  size_t expectedSize = BoardBlockSize()*nBoards();
 
-  if(artdaq_Fragment_.dataSizeBytes()!=(BoardBlockSize()*nBoards())){
+  // If the fragment is compressed we expect 6 fewer bytes for each compressed set of 4 channel diffs
+  // however if there are an odd number of compressions in a sample there are 2 additional bytes
+  if (isCompressed())
+    for(size_t i_b=0; i_b<nBoards(); ++i_b)
+      for(size_t i_s=0; i_s<nSamplesPerChannel(); ++i_s)
+        {
+          size_t nCompressed = std::bitset<16>(CompressionKey(i_b, i_s)).count();
+          expectedSize -= 6*nCompressed - 2*(nCompressed % 2);
+        }
+
+  if(artdaq_Fragment_.dataSizeBytes()!=expectedSize){
     TRACEN("PhysCrateFragment",TLVL_WARNING,"PhysCrateFragment::Verify : Data size not correct!\n\tObserved=%lu, Expected=%lu\n",
 	  artdaq_Fragment_.dataSizeBytes(),
-	  BoardBlockSize()*nBoards());
+	  expectedSize);
     verified=false;
   }
 
@@ -190,4 +213,86 @@ bool icarus::PhysCrateFragment::Verify() const {
   
   return verified;
     
+}
+
+std::vector<uint16_t> icarus::PhysCrateFragment::GenerateKeys(artdaq::Fragment const& f) {
+  size_t nBoards   = f.metadata<icarus::PhysCrateFragmentMetadata>()->num_boards();
+  size_t nSamples  = f.metadata<icarus::PhysCrateFragmentMetadata>()->samples_per_channel();
+
+  std::vector<uint16_t> keys(nBoards*nSamples, 0);
+
+  // if there's no compression it's all uncompressed
+  // NOTE: double check how compression_scheme() is encoded
+  if (f.metadata<icarus::PhysCrateFragmentMetadata>()->compression_scheme() == 0)
+    return keys;
+
+  // loop over the fragment data and check the compression
+  // each board/sample pair gets one 16-bit key
+  // each bit corresponds to a block of 4 channels
+  // 0 means those channels are uncompressed, 1 means they are compressed
+  // if the 4 most significant bits of the 16-bit word is 0x8 the words is compressed
+  size_t cumulativePrevBlockSize = 0;
+  for (size_t b = 0; b < nBoards; b++)
+  {
+    icarus::A2795DataBlock::data_t const* boardData
+                                            = reinterpret_cast<icarus::A2795DataBlock::data_t const*>
+                                                       ( f.dataBeginBytes()
+                                                       + sizeof(icarus::PhysCrateDataTileHeader)
+                                                       + sizeof(icarus::A2795DataBlock::Header)
+                                                       + b*sizeof(icarus::PhysCrateDataTileHeader)
+                                                       + b*4*sizeof(uint16_t)
+                                                       + cumulativePrevBlockSize                   );
+    size_t nWord = 0;
+    for (size_t s = 0; s < nSamples; s++)
+    {
+      uint16_t key = 0;
+      for (size_t bit = 0; bit < 16; bit++)
+      {
+        icarus::A2795DataBlock::data_t word = boardData[nWord];
+        bool isCompressed = ((word & 0xF000) != 0x8000);
+        key += (isCompressed << bit);
+        nWord += (isCompressed) ? 1 : 4;
+      }
+      keys[b*nSamples + s] = key;
+      nWord += (std::bitset<16>(key).count() % 2);
+      cumulativePrevBlockSize += icarus::PhysCrateFragment::SampleBytesFromKey(key);
+    }
+  }
+  return keys;
+}
+
+icarus::PhysCrateFragment::recursionPair icarus::PhysCrateFragment::adc_val_recursive_helper
+  (size_t b, size_t c, size_t s, size_t sTarget, icarus::PhysCrateFragment::recursionPair pair) const
+{
+  // this function helps calculate the ADC values from compressed fragments
+  // it is tail recursive and accumulates the ADC values while finding each ADC difference
+  icarus::A2795DataBlock::data_t runningVal = pair.first;
+  const icarus::A2795DataBlock::data_t* loc = pair.second;
+  const size_t cSet = c / 4;
+  const size_t channelInSet = c % 4;
+  uint16_t sampleKey = this->CompressionKey(b, s);
+
+  size_t cIndex = 4*cSet - 3*std::bitset<16>(sampleKey % (1 << cSet)).count();
+  bool isChannelSetCompressed = ((sampleKey >> cSet) & 0x0001) == 0x0001;
+
+  if (isChannelSetCompressed)
+  {
+    icarus::A2795DataBlock::data_t dataSlice = *(loc+cIndex);
+    icarus::A2795DataBlock::data_t fourBitDiff = (dataSlice >> (4*channelInSet)) & 0x000F;
+    bool isNeg = (fourBitDiff >> 3);
+    runningVal += isNeg*0xFFF0 + fourBitDiff;
+  } else {
+    icarus::A2795DataBlock::data_t dataSlice = *(loc+cIndex+channelInSet);
+    icarus::A2795DataBlock::data_t twelveBitDiff = dataSlice & 0x0FFF;
+    bool isNeg = (s != 0) && (twelveBitDiff >> 11);
+    runningVal += isNeg*0xF000 + twelveBitDiff;
+  }
+
+  size_t keyCount = std::bitset<16>(sampleKey).count();
+  size_t increment = 64 - 3*keyCount + (keyCount % 2);
+
+  if (s == sTarget)
+    return std::make_pair(runningVal, loc + increment);
+
+  return this->adc_val_recursive_helper(b, c, s + 1, sTarget, std::make_pair(runningVal, loc + increment));
 }
