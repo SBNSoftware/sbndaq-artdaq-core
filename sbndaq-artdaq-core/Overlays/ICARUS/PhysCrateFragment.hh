452d1d3130de33ea45099605f5d5cd52fd3c900a
miquelnebot <miquelnebot@users.noreply.github.com>
2024-01-26 11:40:18 -0600

    hotfix

--- sbndaq-artdaq-core/Overlays/ICARUS/PhysCrateFragment.hh
+++ sbndaq-artdaq-core/Overlays/ICARUS/PhysCrateFragment.hh
@@ -7,7 +7,7 @@
 #include <bitset>
 #include <iostream>
 #include <vector>
-
+#include <array>
 #include "sbndaq-artdaq-core/Overlays/ICARUS/structures.h"
 
 // Implementation of "PhysCrateFragment", an artdaq::Fragment overlay class
