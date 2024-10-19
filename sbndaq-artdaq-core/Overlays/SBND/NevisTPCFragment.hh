81958e1af86a1b43a213644a5b2078e588b2f91b
sbnd <sbnd@fnal.gov>
2024-10-19 13:09:16 -0500

    add uncorrected frame to fragment metadata

--- sbndaq-artdaq-core/Overlays/SBND/NevisTPCFragment.hh
+++ sbndaq-artdaq-core/Overlays/SBND/NevisTPCFragment.hh
@@ -25,28 +25,36 @@ class sbndaq::NevisTPCFragmentMetadata {
   
 private:
   uint32_t _event_number;
+  uint32_t _frame_number;
   uint32_t _samples_per_channel;
   uint32_t _n_channels;
   bool     _is_compressed;
+  uint32_t _uncorrected_frame_number;
   
 public:
   NevisTPCFragmentMetadata(){}
-  NevisTPCFragmentMetadata(uint32_t en, uint32_t n_ch, uint32_t n_samples, bool compressed){
+  NevisTPCFragmentMetadata(uint32_t en, uint32_t fn, uint32_t n_ch, uint32_t n_samples, bool compressed, uint32_t ufn){
     _event_number = en;
+    _frame_number = fn;
     _n_channels = n_ch;
     _samples_per_channel = n_samples;
     _is_compressed = compressed;
+    _uncorrected_frame_number = ufn;
   }
   
   uint32_t const& EventNumber()		const { return _event_number; }
+  uint32_t const& FrameNumber()         const { return _frame_number; }
   uint32_t const& NChannels()           const { return _n_channels; }
   uint32_t const& SamplesPerChannel()   const { return _samples_per_channel; }
   bool     const& IsCompressed()        const { return _is_compressed; }
+  uint32_t const& UncorrFrameNumber()   const { return _uncorrected_frame_number; }
   
   void SetEventNumber(uint32_t en) { _event_number = en; }
+  void SetFrameNumber(uint32_t fn) { _frame_number = fn; }
   void SetNChannels(uint32_t nc) { _n_channels = nc; }
   void SetSamplesPerChannel(uint32_t ns) { _samples_per_channel = ns; }
   void SetIsCompressed(bool c) { _is_compressed = c; }
+  void SetUncorrFrameNumber(uint32_t ufn) { _uncorrected_frame_number = ufn; }
 
   size_t ExpectedDataSize() const {
     return (sizeof(NevisTPCHeader)+(sizeof(NevisTPC_ADC_t)*_samples_per_channel*_n_channels));
