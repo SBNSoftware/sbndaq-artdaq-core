fe91271876287e70f012037f19b5df7dfec5d94d
Max Dubnowski <90648380+maxdubnowski@users.noreply.github.com>
2026-01-26 15:19:42 -0500

    Update to version 5

--- sbndaq-artdaq-core/Overlays/SBND/PTB_content.h
+++ sbndaq-artdaq-core/Overlays/SBND/PTB_content.h
@@ -22,7 +22,8 @@ namespace ptb {
 	// version 2: 8 bit gate counter added (Added gate counter and prev timestamp, words are now 192 bits long)
 	// version 3: 8 bit counter overlapped too many times, so we increased to 16 bit gate counter
 	// version 4: HLT 6 was overwriting HLT 2 POT information at beginning of Run 2, so we replaced ordering
-    const int version = 4; 
+	// version 5: HLT 22 was being updated by LLT 30, changed to increment on HLT 27
+    const int version = 5; 
 
     using std::size_t ;
 
